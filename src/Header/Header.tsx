1b8580e297045ee886ecec7e6681c45856cff9f3
Andrea <andrea.rinaldi91@gmail.com>
2025-06-12 23:03:23 +0200

    Remove social links from Footer and navigation items from Header for a cleaner layout

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -6,8 +6,6 @@ const Header: React.FC = () => (
     <nav>
       <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="/articles">Articles</a></li>
-        <li><a href="/about">About</a></li>
       </ul>
       <button className="dark-mode-toggle" aria-label="Toggle dark mode">
         Theme
