37cba55b453d3377eceaf4c562984f9fe17a92e8
Andrea <andrea.rinaldi91@gmail.com>
2025-07-06 13:07:41 +0200

    Move ImagePreview

--- src/Gallery/ImagePreview/ImagePreview.tsx
+++ src/Gallery/ImagePreview/ImagePreview.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./ImagePreview.module.css";
-import { type GalleryImageWithMetadata } from "./GalleryUtils";
+import { type GalleryImageWithMetadata } from "../GalleryUtils";
 
 interface ImagePreviewProps {
   image: GalleryImageWithMetadata;
