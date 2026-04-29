2b9fa1844fc141c158b203c879b54f67111a9841
Andrea <enigmatico.tessuto-0m@icloud.com>
2026-04-29 09:13:07 +0200

    refactor: remove redundant prepare step and prepareTemplates.js

--- scripts/prepareTemplates.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const fs = require('fs');
-const pages = ['index.html', 'about.html', 'articles.html', 'article.html'];
-pages.forEach(f => {
-  const templatePath = `templates/${f}`;
-  if (fs.existsSync(templatePath)) {
-    fs.copyFileSync(templatePath, f);
-  }
-});
